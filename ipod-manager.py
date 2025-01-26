6b8fdb439bb4e910466dbb8aa958e71320f6ad7b
blackbunt <blackbunt@posteo.se>
2025-01-26 23:23:28 +0100

    include header description

--- ipod-manager.py
+++ ipod-manager.py
@@ -1,8 +1,19 @@
-'''
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
+==================================================================
+ iPod Manager - Main Program
+==================================================================
+ File        : ipod-manager.py
+ Description : The main entry point for the iPod Manager tool.
+               Provides a menu-driven interface to manage music
+               on iPods running Rockbox and scrobble play history
+               to Last.fm.
+ Author      : blackbunt
+ Created     : 2025-01-26
+ License     : GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
+ Repository  : https://github.com/blackbunt/ipod-manager
+==================================================================
+"""
+
 import os
 from pathlib import Path
 from InquirerPy import inquirer
--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-#
-#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
+# ==================================================================
+# iPod Manager - Environment Setup Script
+# ==================================================================
+# File        : start.sh
+# Description : This script ensures the necessary Python virtual
+#               environment and dependencies are in place for the
+#               iPod Manager application. It also runs the main
+#               Python program.
+# Author      : blackbunt
+# Created     : 2025-01-26
+# License     : GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
+# Repository  : https://github.com/blackbunt/ipod-manager
+# ==================================================================
+
 # Check and create the virtual environment if it doesn't exist
 if [ ! -d ".venv" ]; then
     echo "Virtual environment (.venv) not found. Creating one..."
