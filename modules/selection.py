c0abca9ff588d3da5561c052b4c51d456234481c
blackbunt <blackbunt@posteo.se>
2025-01-26 23:24:19 +0100

    added description

--- modules/selection.py
+++ modules/selection.py
@@ -1,8 +1,18 @@
-'''
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
+==================================================================
+ iPod Manager - Submodule
+==================================================================
+ File        : selection.py
+ Description : Provides functions for discovering connected iPods,
+               prompting the user to select an iPod or artists, 
+               and listing artists in the music library.
+               This submodule is part of the iPod Manager project.
+ Author      : blackbunt
+ Created     : 2025-01-26
+ License     : GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
+ Repository  : https://github.com/blackbunt/ipod-manager
+==================================================================
+"""
 import os
 from pathlib import Path
 from typing import List
--- modules/utils.py
+++ modules/utils.py
@@ -1,8 +1,18 @@
-'''
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
+==================================================================
+ iPod Manager - Submodule
+==================================================================
+ File        : utils.py
+ Description : Provides utility functions for handling the music 
+               folder, identifying devices for mountpoints, listing 
+               blocking processes, and safely unmounting iPods.
+               This submodule is part of the iPod Manager project.
+ Author      : blackbunt
+ Created     : 2025-01-26
+ License     : GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
+ Repository  : https://github.com/blackbunt/ipod-manager
+==================================================================
+"""
 import subprocess
 from pathlib import Path
 from InquirerPy import inquirer
